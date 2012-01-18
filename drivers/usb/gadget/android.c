8ff2830a7623ad6f011680edab8d6f778dd6b0c1
Scott Anderson <saa@google.com>
2012-01-18 15:56:51 -0800

    usb: gadget: android: Honor CONFIG_USB_GADGET_VBUS_DRAW
    The maximum current draw was hard coded to 500 mA.  composite.c
    has code that uses CONFIG_USB_GADGET_VBUS_DRAW to set the
    bMaxPower and to set whether or not the device is self-powered if
    they haven't been set.  This change removes the code in android.c
    to allow composite.c to set them.
    
    (cherry picked from commit 09701e3edf03f92f4215aad83b32cd8cec7fb689 from
    android.googlesource.com/common.git)
    
    Change-Id: I9db37922e91ee86e9e5c0e14519e119e5c41ca48
    Signed-off-by: Scott Anderson <saa@google.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/78889
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -181,8 +181,6 @@ static struct usb_configuration android_config_driver = {
 	.label		= "android",
 	.unbind		= android_unbind_config,
 	.bConfigurationValue = 1,
-	.bmAttributes	= USB_CONFIG_ATT_ONE | USB_CONFIG_ATT_SELFPOWER,
-	.bMaxPower	= 0xFA, /* 500ma */
 };
 
 static int mass_storage_function_set_cdrom_lun(char *lunpath);
@@ -1591,7 +1589,6 @@ static int android_bind(struct usb_composite_dev *cdev)
 		device_desc.bcdDevice = __constant_cpu_to_le16(0x9999);
 	}
 
-	usb_gadget_set_selfpowered(gadget);
 	dev->cdev = cdev;
 
 	return 0;
