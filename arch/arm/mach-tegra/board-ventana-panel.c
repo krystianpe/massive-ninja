02f9bcec0e68e81ff514676a43e903db77b16dac
Pritesh Raithatha <praithatha@nvidia.com>
2012-03-13 17:17:46 +0530

    arm: tegra: ventana: use fixed regulator instead of direct gpio
    Bug 925547
    
    Change-Id: I81f87cef3a9767d9bd60b72e33a23620392ab5fc
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/89736
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-ventana-panel.c
+++ arch/arm/mach-tegra/board-ventana-panel.c
@@ -38,7 +38,6 @@
 #include "board.h"
 #include "tegra2_host1x_devices.h"
 
-#define ventana_pnl_pwr_enb	TEGRA_GPIO_PC6
 #define ventana_bl_enb		TEGRA_GPIO_PD4
 #define ventana_lvds_shutdown	TEGRA_GPIO_PB2
 #define ventana_hdmi_hpd	TEGRA_GPIO_PN7
@@ -47,6 +46,8 @@
 /*panel power on sequence timing*/
 #define ventana_pnl_to_lvds_ms	0
 #define ventana_lvds_to_bl_ms	200
+
+static struct regulator *pnl_pwr;
 
 #ifdef CONFIG_TEGRA_DC
 static struct regulator *ventana_hdmi_reg = NULL;
@@ -113,7 +114,17 @@ static int ventana_panel_enable(void)
 		regulator_put(reg);
 	}
 
-	gpio_set_value(ventana_pnl_pwr_enb, 1);
+	if (pnl_pwr == NULL) {
+		pnl_pwr = regulator_get(NULL, "pnl_pwr");
+		if (WARN_ON(IS_ERR(pnl_pwr)))
+			pr_err("%s: couldn't get regulator pnl_pwr: %ld\n",
+				__func__, PTR_ERR(pnl_pwr));
+		else
+			regulator_enable(pnl_pwr);
+	} else {
+		regulator_enable(pnl_pwr);
+	}
+
 	mdelay(ventana_pnl_to_lvds_ms);
 	gpio_set_value(ventana_lvds_shutdown, 1);
 	mdelay(ventana_lvds_to_bl_ms);
@@ -123,7 +134,7 @@ static int ventana_panel_enable(void)
 static int ventana_panel_disable(void)
 {
 	gpio_set_value(ventana_lvds_shutdown, 0);
-	gpio_set_value(ventana_pnl_pwr_enb, 0);
+	regulator_disable(pnl_pwr);
 	return 0;
 }
 
@@ -384,10 +395,6 @@ int __init ventana_panel_init(void)
 {
 	int err;
 	struct resource __maybe_unused *res;
-
-	gpio_request(ventana_pnl_pwr_enb, "pnl_pwr_enb");
-	gpio_direction_output(ventana_pnl_pwr_enb, 1);
-	tegra_gpio_enable(ventana_pnl_pwr_enb);
 
 	gpio_request(ventana_lvds_shutdown, "lvds_shdn");
 	gpio_direction_output(ventana_lvds_shutdown, 1);
