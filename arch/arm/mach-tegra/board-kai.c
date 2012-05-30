403e34de578840d1ba8d98a64676bb5feea627a5
Hunk Lin <hulin@nvidia.com>
2012-05-30 13:56:29 +0800

    arm: tegra: kai: disable hotplug for usb instance 1
    Kai's USB1 instance is used for modem, so disabling
    the hotplug.
    
    Bug 989441
    Bug 989400
    
    Change-Id: Ia1e84a727f8343a55d6afc5360b35e5afed6947c
    Signed-off-by: Hunk Lin <hulin@nvidia.com>
    Reviewed-on: http://git-master/r/105315
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Rakesh Bodla <rbodla@nvidia.com>
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    
    Conflicts:
    
    	arch/arm/mach-tegra/board-kai.c

--- arch/arm/mach-tegra/board-kai.c
+++ arch/arm/mach-tegra/board-kai.c
@@ -743,11 +743,18 @@ static struct tegra_usb_platform_data tegra_ehci1_utmi_pdata = {
 	},
 };
 
-#ifdef CONFIG_USB_SUPPORT
-static struct usb_phy_plat_data tegra_usb_phy_pdata[] = {
-	[0] = {
-			.instance = 0,
-			.vbus_gpio = -1,
+static struct tegra_usb_platform_data tegra_ehci2_utmi_pdata = {
+	.port_otg = false,
+	.has_hostpc = true,
+	.phy_intf = TEGRA_USB_PHY_INTF_UTMI,
+	.op_mode	= TEGRA_USB_OPMODE_HOST,
+	.u_data.host = {
+		.vbus_gpio = -1,
+		.vbus_reg = NULL,
+		.hot_plug = false,
+		.remote_wakeup_supported = true,
+		.power_off_on_suspend = true,
+
 	},
 	.u_cfg.utmi = {
 		.hssync_start_delay = 0,
