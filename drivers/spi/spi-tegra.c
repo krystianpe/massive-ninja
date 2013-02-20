43da56d72e4956f4ab66f88632f8e3f722a1192b
Krystian <krystian@krystian-VirtualBox.(none)>
2013-02-19 19:51:19 -0500

    cpcap finally starts
    Conflicts:
    	drivers/spi/spi-tegra.c

--- drivers/spi/spi-tegra.c
+++ drivers/spi/spi-tegra.c
@@ -932,8 +932,10 @@ static int spi_tegra_setup(struct spi_device *spi)
 	unsigned long val;
 	unsigned long flags;
 
-	dev_dbg(&spi->dev, "setup %d bpw, %scpol, %scpha, %dHz\n",
+	
+	dev_info(&spi->dev, "setup %d bpw, %s, %scpol, %scpha, %dHz\n",
 		spi->bits_per_word,
+		(spi->mode & SPI_CS_HIGH) ? "cs_high, " : "",
 		spi->mode & SPI_CPOL ? "" : "~",
 		spi->mode & SPI_CPHA ? "" : "~",
 		spi->max_speed_hz);
@@ -961,7 +963,11 @@ static int spi_tegra_setup(struct spi_device *spi)
 	}
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+	dev_info(&spi->dev, "setup chipselect %d (0x%x)\n", spi->chip_select, cs_bit);
+>>>>>>> 2286fd9... cpcap finally starts
 	pm_runtime_get_sync(&tspi->pdev->dev);
 	tegra_spi_clk_enable(tspi);
 
