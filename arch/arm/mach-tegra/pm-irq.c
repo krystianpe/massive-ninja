f1e6b992d9ca519488cb57cb874bd2e8da1d0147
Krystian <krystian@krystian-VirtualBox.(none)>
2013-02-23 19:10:53 -0500

    cpcap fixes, more needed to finally get USB working

--- arch/arm/mach-tegra/pm-irq.c
+++ arch/arm/mach-tegra/pm-irq.c
@@ -173,12 +173,12 @@ int tegra_pm_irq_set_wake(int irq, int enable)
 =======
 		tegra_lp0_wake_enb |= (wake_msk.wake_mask_hi |
 			wake_msk.wake_mask_lo | wake_msk.wake_mask_any);
-		printk("%s: Enabling wake %d", __func__, irq);
+		printk("%s: Enabling wake %d\n", __func__, err);
 	
 	} else {
 		tegra_lp0_wake_enb &= ~(wake_msk.wake_mask_hi |
 			wake_msk.wake_mask_lo | wake_msk.wake_mask_any);
-		printk("%s: Disabling wake %d", __func__, irq);
+		printk("%s: Disabling wake %d\n", __func__, err);
 	}
 >>>>>>> e0e91a5... cpcap debuging
 	return 0;
